30510d3432fe612dea978ca40ea55f805439aeef
Ciro García Belmonte <info@cirogarcia.dev>
2026-02-02 18:21:03 +0100

    Add functionality to launch Chromium in kiosk mode and create systemd service

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 var templates map[string]*template.Template
@@ -29,7 +32,34 @@ func main() {
 			http.FileServer(http.Dir("static"))))
 
 	log.Println("Meet room control listening on :5000")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go openChromium(ctx)
 	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func openChromium(ctx context.Context) {
+	cmd := exec.CommandContext(ctx,
+		"chromium",
+		"--kiosk",
+		"--noerrdialogs",
+		"--disable-infobars",
+		"--disable-session-crashed-bubble",
+		"--autoplay-policy=no-user-gesture-required",
+		"--usenable-features=VaapiVideoDecoder",
+		"--remote-debugging-port=9222",
+		"http://localhost:5000/idle",
+	)
+	cmd.Env = append(os.Environ(), "DISPLAY=:0")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatalf("Failed to start chromium: %v", err)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatalf("Chromium exited with error: %v", err)
+	}
+	log.Println("Chromium exited")
 }
 
 func render(w http.ResponseWriter, name string, data any) {
